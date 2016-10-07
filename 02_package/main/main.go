79962404e67caac8f238e3569e248765c5cc2210
associatesgo <associatesgo@gmail.com>
2016-10-07 13:03:06 -0700

    modified main.go

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	"github.com/Associatesgo/udemy Training/02_package/stringutil/
+	"github.com/Associatesgo/udemy Training/02_package/icomefromalaska/
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
